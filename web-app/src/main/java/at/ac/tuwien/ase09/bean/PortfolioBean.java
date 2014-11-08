255df1087ef7e1fc41ec6a23d28aa4f773e3445d
lung <e1026620@student.tuwien.ac.at>
2014-11-08 15:48:50 +0100

    Started with portfolio detail view

---
+++ web-app/src/main/java/at/ac/tuwien/ase09/bean/PortfolioBean.java
@@ -0,0 +1,31 @@
+package at.ac.tuwien.ase09.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import at.ac.tuwien.ase09.model.Portfolio;
+import at.ac.tuwien.ase09.service.PortfolioService;
+
+@Named
+@ViewScoped
+public class PortfolioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private PortfolioService portfolioService;
+
+	
+	
+	public List<Portfolio> getPortfolios() {
+		return portfolioService.getPortfolios();
+	}
+
+		
+	
+}
