1fac0270d0ff23faa52cfd46e56bd1bf4a5a12d0
lung <e1026620@student.tuwien.ac.at>
2014-11-12 16:31:59 +0100

    Added missing entity annotation

--- model/src/main/java/at/ac/tuwien/ase09/model/order/MarketOrder.java
+++ model/src/main/java/at/ac/tuwien/ase09/model/order/MarketOrder.java
@@ -1,8 +1,10 @@
 package at.ac.tuwien.ase09.model.order;
 
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import javax.persistence.Transient;
 
+@Entity
 @DiscriminatorValue(OrderType.TYPE_MARKET)
 public class MarketOrder extends Order {
 	private static final long serialVersionUID = 1L;
