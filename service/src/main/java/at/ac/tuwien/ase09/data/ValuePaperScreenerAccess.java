8c13b18e3f94272981946f33b9f189a5dd4d0fd6
Walla1 <klaus.walla@aon.at>
2014-11-16 19:41:39 +0100

    + ValuePaperScreenerAccess #37916 + ValuePaperScreenerService #37916

---
+++ service/src/main/java/at/ac/tuwien/ase09/data/ValuePaperScreenerAccess.java
@@ -0,0 +1,58 @@
+package at.ac.tuwien.ase09.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import at.ac.tuwien.ase09.model.ValuePaper;
+
+@Stateless
+public class ValuePaperScreenerAccess {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	public List<ValuePaper> findByValuePaper(ValuePaper valuePaper, Boolean isTypeSecificated) {
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<ValuePaper> query = builder.createQuery(ValuePaper.class);
+		Root<ValuePaper> rootValuePaper = query.from(ValuePaper.class);
+		
+		List<Predicate> wherePredicates = new ArrayList<Predicate>();
+		
+		if (valuePaper.getName() != null) {
+			String na = valuePaper.getName().replace('*', '%').replace('?', '_').toUpperCase();
+			wherePredicates.add( builder.like( builder.upper( rootValuePaper.<String>get("c_name") ), na) );
+		}
+		
+		if (valuePaper.getCurrency() != null) {
+			wherePredicates.add( builder.equal(rootValuePaper.get("c_currency") , valuePaper.getCurrency()));
+		}
+		
+		if (valuePaper.getIsin() != null) {
+			wherePredicates.add( builder.equal(rootValuePaper.get("c_isin"), valuePaper.getIsin()));
+		}
+
+		if (valuePaper.getCountry() != null) {
+			String co = valuePaper.getCountry().replace('*', '%').replace('?', '_').toUpperCase();
+			wherePredicates.add( builder.like( builder.upper( rootValuePaper.<String>get("c_country") ), co) );
+		}
+		if(isTypeSecificated)
+		{
+			wherePredicates.add( builder.equal(rootValuePaper.get("c_valuepaper_type"), valuePaper.getType()) );
+		}
+		
+		Predicate whereClause = builder.and(wherePredicates.toArray(new Predicate[0]));
+		
+		query.where(whereClause);
+		
+		return em.createQuery(query).getResultList();
+		
+	}
+}
