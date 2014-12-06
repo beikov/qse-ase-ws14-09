228f0411dc23b402fa5c32b1d1d13dd78ebd6e62
manuel <kruisz88@gmail.com>
2014-12-06 12:46:45 +0100

    implemented some PortfolioDataAccess Tests

--- service/src/test/java/at/ac/tuwien/ase09/test/data/PortfolioDataAccessTest.java
+++ service/src/test/java/at/ac/tuwien/ase09/test/data/PortfolioDataAccessTest.java
@@ -4,9 +4,15 @@ import static org.junit.Assert.*;
 
 import javax.inject.Inject;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import at.ac.tuwien.ase09.data.PortfolioDataAccess;
+import at.ac.tuwien.ase09.model.Portfolio;
+import at.ac.tuwien.ase09.model.PortfolioVisibility;
+import at.ac.tuwien.ase09.model.User;
+import at.ac.tuwien.ase09.service.PortfolioService;
+import at.ac.tuwien.ase09.service.UserService;
 import at.ac.tuwien.ase09.test.AbstractContainerTest;
 import at.ac.tuwien.ase09.test.DatabaseAware;
 
@@ -16,27 +22,46 @@ public class PortfolioDataAccessTest extends AbstractContainerTest<ValuePaperDat
 	private static final long serialVersionUID = 1L;
 
 	@Inject
+	private PortfolioService portfolioService;
+	
+	@Inject
 	private PortfolioDataAccess portfolioDataAccess;
+
+	@Inject
+	private UserService userService;
+	
+	private Portfolio portfolio;
+	private User owner;
+	
+	@Before
+	public void init(){
+		portfolio = new Portfolio();
+		portfolio.setVisibility(new PortfolioVisibility());
+		portfolio.setName("pf1");
+		
+		owner = new User();
+		owner.setUsername("owner");
+		userService.saveUser(owner);
+		
+		portfolio.setOwner(owner);
+		portfolioService.savePortfolio(portfolio);
+	}
+
 	
 	@Test
-	public void test_getPortfolioByNameForUser_withCorrectInputs_returnsPortfolio(){
-		//todo
-		fail();
+	public void test_getPortfolioByNameForUser_withCorrectInputs(){
+		assertEquals(portfolio, portfolioDataAccess.getPortfolioByNameForUser("pf1", owner));
 	}
 	
-	public void test_getPortfolioByNameForUser__withNonExistingUser(){
-		//todo
-		fail();
+	@Test
+	public void test_getPortfolioByNameForUser__withWrongName(){
+		assertEquals(null,portfolioDataAccess.getPortfolioByNameForUser("wrong_name", owner));
 	}
 	
-	public void test_getPortfolioByNameForUser__withNonExistingPortfolioName(){
-		//todo
-		fail();
-	}
 	
+	@Test
 	public void test_getPortfolioByNameForUser__withNonNullInputs(){
-		//todo
-		fail();
+		assertEquals(null, portfolioDataAccess.getPortfolioByNameForUser(null, null));
 	}	
 		
 
