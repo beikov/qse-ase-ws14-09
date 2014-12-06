e7c616cf495c58448df1781d598c70d658843a74
lung <e1026620@student.tuwien.ac.at>
2014-12-06 20:08:14 +0100

    valuePaper performance update

--- service/src/main/java/at/ac/tuwien/ase09/service/PortfolioService.java
+++ service/src/main/java/at/ac/tuwien/ase09/service/PortfolioService.java
@@ -211,7 +211,18 @@ public class PortfolioService {
 	}
 
 	public double getChange(PortfolioValuePaper pvp) {
-		double payed = getTotalPayedForValuePaper(pvp.getValuePaper().getCode()).doubleValue();
+		
+		double payed = 0.0;
+		
+		for (TransactionEntry t : pvp.getPortfolio().getTransactionEntries()) {
+			if (t.getType() == TransactionType.ORDER) {	
+				if (((OrderTransactionEntry)t).getOrder().getValuePaper().getCode().equals(pvp.getValuePaper().getCode())) {					
+					payed += t.getValue().getValue().doubleValue();
+				}
+			}
+		}
+		//payed = getTotalPayedForValuePaper(pvp.getValuePaper().getCode()).doubleValue();
+		
 		int volume = pvp.getVolume();
 		double latestPrice = priceDataAccess.getLastPriceEntry(pvp.getValuePaper().getCode()).getPrice().doubleValue();
 		
