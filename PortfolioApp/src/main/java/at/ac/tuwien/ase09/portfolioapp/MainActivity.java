a0390ff6062769a165ea71a5a2c6d0af1510c921
Lukas Auer <nykralus@gmx.at>
2014-12-09 09:37:24 +0100

    app commit

--- PortfolioApp/src/main/java/at/ac/tuwien/ase09/portfolioapp/MainActivity.java
+++ PortfolioApp/src/main/java/at/ac/tuwien/ase09/portfolioapp/MainActivity.java
@@ -1,36 +1,98 @@
 package at.ac.tuwien.ase09.portfolioapp;
 
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements TabListener {
+
+    private List<Fragment> fragList = new ArrayList<Fragment>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
+        super.onCreate(savedInstanceState);
+
+        ActionBar bar = getActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+
+
+        Tab tab = bar.newTab();
+        tab.setText("Performance");
+        tab.setTabListener(this);
+        bar.addTab(tab);
+
+        tab = bar.newTab();
+        tab.setText("Order");
+        tab.setTabListener(this);
+        bar.addTab(tab);
+
+        tab = bar.newTab();
+        tab.setText("Transaktionen");
+        tab.setTabListener(this);
+        bar.addTab(tab);
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+
     }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        Fragment f = null;
+        PortfolioViewFragment tf = null;
+
+        if (fragList.size() > tab.getPosition())
+            fragList.get(tab.getPosition());
+
+        if (f == null) {
+            tf = new PortfolioViewFragment();
+            Bundle data = new Bundle();
+            data.putInt("idx",  tab.getPosition());
+            tf.setArguments(data);
+            fragList.add(tf);
+        }
+        else
+            tf = (PortfolioViewFragment) f;
+
+        ft.replace(android.R.id.content, tf);
+
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        if (fragList.size() > tab.getPosition()) {
+            ft.remove(fragList.get(tab.getPosition()));
+        }
+
+    }
+
+
+
+
+
+
+
 }
