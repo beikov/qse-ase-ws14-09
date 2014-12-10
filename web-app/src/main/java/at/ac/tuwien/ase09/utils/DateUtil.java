1f8bb7e23466d1a63398ef014369e897e06e5ab4
lung <e1026620@student.tuwien.ac.at>
2014-12-11 00:24:07 +0100

    changed dateUtil bean

--- web-app/src/main/java/at/ac/tuwien/ase09/utils/DateUtil.java
+++ web-app/src/main/java/at/ac/tuwien/ase09/utils/DateUtil.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.ase09.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -10,7 +11,7 @@ import javax.inject.Named;
 @ApplicationScoped
 public class DateUtil {
   
-	public static String formatDate(String pattern, Date date) {
+	public static String formatDate(Date date, String pattern) {
 		if (date == null || pattern == null) {
 			return "";
 		}
@@ -18,10 +19,22 @@ public class DateUtil {
 	}
 	
 	public static String formatDate(Date date) {
-		return DateUtil.formatDate("dd.MM.yyyy", date);
+		return DateUtil.formatDate(date, "dd.MM.yyyy");
 	}
 	
 	public static String formatDateWithTime(Date date) {
-		return DateUtil.formatDate("dd.MM.yyyy - hh:mm:ss", date);
+		return DateUtil.formatDate(date, "dd.MM.yyyy - hh:mm:ss");
+	}
+	
+	public static String formatCalendar(Calendar calendar, String pattern) {
+		return DateUtil.formatDate(calendar.getTime(), pattern);
+	}
+	
+	public static String formatCalendar(Calendar calendar) {
+		return DateUtil.formatDate(calendar.getTime());
+	}
+	
+	public static String formatCalendarWithTime(Calendar calendar) {
+		return DateUtil.formatDateWithTime(calendar.getTime());
 	}
 }
