8218734e0e0435a5242d05c70ece9bc752a8bd48
Walla1 <klaus.walla@aon.at>
2014-12-18 23:32:49 +0100

    + UserContext Stockmarketgame

--- web-app/src/main/java/at/ac/tuwien/ase09/bean/StockMarketGameViewBean.java
+++ web-app/src/main/java/at/ac/tuwien/ase09/bean/StockMarketGameViewBean.java
@@ -4,9 +4,11 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import at.ac.tuwien.ase09.context.WebUserContext;
 import at.ac.tuwien.ase09.data.StockMarketGameDataAccess;
 import at.ac.tuwien.ase09.exception.EntityNotFoundException;
 import at.ac.tuwien.ase09.model.StockMarketGame;
+import at.ac.tuwien.ase09.model.User;
 import at.ac.tuwien.ase09.model.ValuePaper;
 
 @Named
@@ -15,13 +17,23 @@ public class StockMarketGameViewBean {
 
 	@Inject
 	private StockMarketGameDataAccess stockMarketGameAccess;
+	@Inject
+	private WebUserContext userContext;
 	
 	private Long gameID;
 	private boolean adminLoggedIn;
+	private User user;
 	private StockMarketGame stockMarketGame;
 	
 	public void init(){
+		User user=userContext.getUser();
 		loadStockMarketGame(gameID);
+		checkAdminLoggedIn();
+		
+	}
+	private void checkAdminLoggedIn() {
+		if(stockMarketGame!=null)
+			adminLoggedIn=user.equals(stockMarketGame.getOwner().getAdmin());
 		
 	}
 	private void loadStockMarketGame(Long gameID) {
