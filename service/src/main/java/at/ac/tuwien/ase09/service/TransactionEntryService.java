9db4c0980b729381f2daed2852046da7c59c6f2b
Christian Beikov <christian.beikov@gmail.com>
2015-01-28 23:44:40 +0100

    Fixed notifications

--- service/src/main/java/at/ac/tuwien/ase09/service/TransactionEntryService.java
+++ service/src/main/java/at/ac/tuwien/ase09/service/TransactionEntryService.java
@@ -9,6 +9,7 @@ import javax.ejb.Stateless;
 import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.enterprise.event.Observes;
 import javax.enterprise.event.TransactionPhase;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
 import at.ac.tuwien.ase09.event.Added;
@@ -24,22 +25,28 @@ public class TransactionEntryService extends AbstractService {
 	private NotificationService notificationService;
 	@Resource
 	private ManagedExecutorService managedExecutorService;
+	@Inject
+	private Instance<TransactionEntryService> self;
 
 	public void onPriceEntryAdded(@Observes(during = TransactionPhase.AFTER_COMPLETION) @Added TransactionEntry entry) {
 		managedExecutorService.submit(() -> {
-			Portfolio p = em.createQuery("SELECT p FROM Portfolio p LEFT JOIN FETCH p.followers JOIN FETCH p.owner o LEFT JOIN FETCH o.followers WHERE p.id = :portfolioId", Portfolio.class)
-				.setParameter("portfolioId", entry.getPortfolio().getId())
-				.getSingleResult();
-			Set<User> receipents = new HashSet<>();
-			receipents.addAll(p.getFollowers());
-			receipents.addAll(p.getOwner().getFollowers());
-			
-			for (User u : receipents) {
-				FollowerTransactionAddedNotification n = new FollowerTransactionAddedNotification();
-				n.setTransactionEntry(entry);
-				n.setUser(u);
-				notificationService.addNotification(n);
-			}
+			self.get().notifyFollowers(entry);
 		});
+	}
+	
+	public void notifyFollowers(TransactionEntry entry) {
+		Portfolio p = em.createQuery("SELECT p FROM Portfolio p LEFT JOIN FETCH p.followers JOIN FETCH p.owner o LEFT JOIN FETCH o.followers WHERE p.id = :portfolioId", Portfolio.class)
+			.setParameter("portfolioId", entry.getPortfolio().getId())
+			.getSingleResult();
+		Set<User> receipents = new HashSet<>();
+		receipents.addAll(p.getFollowers());
+		receipents.addAll(p.getOwner().getFollowers());
+		
+		for (User u : receipents) {
+			FollowerTransactionAddedNotification n = new FollowerTransactionAddedNotification();
+			n.setTransactionEntry(entry);
+			n.setUser(u);
+			notificationService.addNotification(n);
+		}
 	}
 }
